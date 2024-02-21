f5a1bb1a65d5f2de43de07a81c90bde38093b275
RudolfVonStroheim <mgus92896@gmail.com>
2024-02-21 14:09:02 +0300

    Add Entity class

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import pygame
 
-pygame.init
+
+pygame.init()
 size = x, y = 800, 600
 
+
+def load_img(path):
+
+
+
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, coords, path, *group):
+        super().__init__(*group)
+        self.image = load_img(path)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = coords
 
